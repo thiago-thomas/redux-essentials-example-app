2a6a1825dd3104330b27fc9850a88a16b9acc62b
Thiago Thomas <thiago_rocha17@hotmail.com>
2025-09-16 15:54:03 +0100

    Adding User Login:   -  Adding the Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 
+import { useAppSelector } from './app/hooks'
 import { Navbar } from './components/Navbar'
+
+import { LoginPage } from './features/auth/LoginPage'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import { PostsMainPage } from './features/posts/PostsMainPage'
 import { EditPostForm } from './features/posts/EditPostForm'
+
+import { selectCurrentUsername } from './features/auth/authSlice'
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const username = useAppSelector(selectCurrentUsername)
+
+  if (!username) {
+    return <Navigate to="/" replace />
+  }
+
+  return children
+}
 
 function App() {
   return (
@@ -11,9 +26,19 @@ function App() {
       <Navbar />
       <div className="App">
         <Routes>
-          <Route path="/" element={<PostsMainPage />}></Route>
-          <Route path="/posts/:postId" element={<SinglePostPage />}></Route>
-          <Route path="/editPost/:postId" element={<EditPostForm />}></Route>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/*"
+            element={
+              <ProtectedRoute>
+                <Routes>
+                  <Route path="/posts" element={<PostsMainPage />}></Route>
+                  <Route path="/posts/:postId" element={<SinglePostPage />}></Route>
+                  <Route path="/editPost/:postId" element={<EditPostForm />}></Route>
+                </Routes>
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
